924c8efbe5cec046e370dfbd0fb8a9d702e772d4
dozhou <dozhou@rubiconproject.com>
2018-09-13 01:48:07 -0700

    add stack test

--- src/stack/__tests__/animate.unit.test.js
+++ src/stack/__tests__/animate.unit.test.js
@@ -1,5 +1,25 @@
-describe('size method', () => {
-  test('empty stack', () => {
-    expect(0).toBe(0);
+/* eslint-env browser */
+
+import animate from '../animate';
+import Actions from '../Actions';
+
+// const dom = new JSDOM('<!doctype html><html><body><div id="alchemy"></div></body></html>');
+// document.body.innerHTML = '<div id="alchemy">1</div>';
+let actions;
+
+beforeEach(() => {
+  jest.useFakeTimers();
+  actions = new Actions();
+});
+
+describe('Stack animate method', () => {
+  test('animate interval method', () => {
+    const pushSize = 10;
+    for (let i = 0; i < pushSize; i += 1) {
+      actions.push(i);
+    }
+    animate(undefined, actions.actions);
+    // jest.runAllTimers();
+    expect(setInterval).toBeCalled();
   });
 });
--- src/stack/__tests__/index.unit.test.js
+++ src/stack/__tests__/index.unit.test.js
@@ -1,15 +1,65 @@
 /* eslint-env browser */
 
-import Index from '../index';
+import Stack from '../index';
 
-xdescribe('main describe', () => {
-  test('main test', () => {
-    const index = new Index();
-    for (let i = 0; i < 10; i += 1) {
-      index.push(i);
+let stack;
+
+beforeEach(() => {
+  stack = new Stack();
+});
+
+const getLastAction = () => {
+  const array = stack.actions.actions;
+  return array[array.length - 1];
+};
+
+describe('Stack Index Stack basic method', () => {
+  test('stack basic method: push, peek, pop', () => {
+    const pushSize = 100;
+    for (let i = 0; i < pushSize; i += 1) {
+      stack.push(i);
     }
-    document.body.innerHTML = '<div id="alchemy"></div>';
-    const config = { container: document.getElementById('alchemy') };
-    index.translate(config);
+    expect(stack.size).toBe(pushSize);
+    const peeked = stack.peek();
+    const poped = stack.pop();
+    expect(peeked).toBe(pushSize - 1);
+    expect(peeked).toBe(poped);
+  });
+});
+
+describe('Stack Index Actions push, peek, pop method', () => {
+  test('push method', () => {
+    const item = 10;
+    stack.push(item);
+    expect(stack.actions.size).toBe(1);
+    const { action, data } = getLastAction();
+    expect(action).toBe('push');
+    expect(data).toBe(item);
+  });
+
+  test('peek method', () => {
+    stack.peek();
+    expect(stack.actions.size).toBe(1);
+    const { action } = getLastAction();
+    expect(action).toBe('peek');
+  });
+
+  test('pop method', () => {
+    stack.pop();
+    expect(stack.actions.size).toBe(1);
+    const { action } = getLastAction();
+    expect(action).toBe('pop');
+  });
+});
+
+describe('Stack Index show method', () => {
+  test('show/animation test', () => {
+    const pushSize = 10;
+    for (let i = 0; i < pushSize; i += 1) {
+      stack.push(i);
+    }
+    jest.useFakeTimers();
+    stack.show();
+    expect(setInterval).toBeCalled();
   });
 });
